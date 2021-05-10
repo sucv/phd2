2cc8b04f5b992fd5fd91c98152d9a342d1613af0
sucv <gcyyyyn@gmail.com>
2021-05-10 18:44:15 +0700

    0.703

--- project/emotion_analysis_on_mahnob_hci/regression/main.py
+++ project/emotion_analysis_on_mahnob_hci/regression/main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser.add_argument('-high_performance_cluster', default=0, type=int, help='On high-performance server or not?')
     parser.add_argument('-stamp', default='final_correct', type=str, help='To indicate different experiment instances')
     parser.add_argument('-dataset', default='mahnob_hci', type=str, help='The dataset name.')
-    parser.add_argument('-modality', default=['eeg_psd'], nargs="*", help='frame, eeg_image, eeg_raw, eeg_psd')
+    parser.add_argument('-modality', default=['frame'], nargs="*", help='frame, eeg_image, eeg_raw, eeg_psd')
     parser.add_argument('-resume', default=0, type=int, help='Resume from checkpoint?')
 
     parser.add_argument('-num_folds', default=10, type=int, help="How many folds to consider?")
@@ -29,13 +29,13 @@ if __name__ == '__main__':
 
     parser.add_argument('-normalize_eeg_raw', default=0, type=int, help='Whether to normalize eeg raw data?')
     # Models
-    parser.add_argument('-model_name', default="1d_only", help='Model: 2d1d, 2dlstm, eegnet1d, eegnetlstm, 1d_only, lstm_only')
+    parser.add_argument('-model_name', default="2d1d", help='Model: 2d1d, 2dlstm, eegnet1d, eegnetlstm, 1d_only, lstm_only')
     parser.add_argument('-backbone_mode', default="ir", help='Mode for resnet50 backbone: ir, ir_se')
     parser.add_argument('-backbone_state_dict_frame', default="model_state_dict_0.86272", help='The filename for the backbone state dict.')
     parser.add_argument('-backbone_state_dict_eeg', default="mahnob_reg_v", help='The filename for the backbone state dict.')
-    parser.add_argument('-cnn1d_embedding_dim', default=192, type=int, help='Dimensions for temporal convolutional networks feature vectors.')
-    parser.add_argument('-cnn1d_channels', default=[128], nargs="+", type=int, help='The specific epochs to do something.')
-    parser.add_argument('-cnn1d_kernel_size', default=3, type=int, help='The size of the 1D kernel for temporal convolutional networks.')
+    parser.add_argument('-cnn1d_embedding_dim', default=512, type=int, help='Dimensions for temporal convolutional networks feature vectors.')
+    parser.add_argument('-cnn1d_channels', default=[128, 128, 128, 128], nargs="+", type=int, help='The specific epochs to do something.')
+    parser.add_argument('-cnn1d_kernel_size', default=5, type=int, help='The size of the 1D kernel for temporal convolutional networks.')
     parser.add_argument('-cnn1d_dropout', default=0.1, type=float, help='The dropout rate.')
 
     parser.add_argument('-lstm_embedding_dim', default=256, type=int, help='Dimensions for LSTM feature vectors.')
@@ -57,7 +57,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-learning_rate', default=1e-5, type=float, help='The initial learning rate.')
     parser.add_argument('-min_learning_rate', default=1e-6, type=float, help='The minimum learning rate.')
-    parser.add_argument('-num_epochs', default=2, type=int, help='The total of epochs to run during training.')
+    parser.add_argument('-num_epochs', default=30, type=int, help='The total of epochs to run during training.')
     parser.add_argument('-min_num_epochs', default=0, type=int, help='The minimum epoch to run at least.')
     parser.add_argument('-time_delay', default=0, type=float, help='The time delay between input and label, in seconds.')
     parser.add_argument('-early_stopping', default=20, type=int, help='If no improvement, the number of epoch to run before halting the training')
